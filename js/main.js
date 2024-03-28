0083493c2b2102a161a5d8bacadad06c869903b9
Jose Emilio Tessaro <emilio_tessaro@live.com.ar>
2024-03-28 11:18:08 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,63 @@
 // Pre Entrega II
 
 // Definición de varialbeles
+let nombreIngresado;
+let codigoProd;
+let descripcion;
+let nacionalidad;
+let cantidad;
+let opcionIngreso;
+let accion;
+let ingreseNac;
 
-let codigo = {};
-let descripcion = {};
-let nacionalidad = {};
-let cantidad = {};
-let opcionIngreso = {};
-let accion = {};
-let i = 0
 
-// Defiición de Arrays
-const arraysCodProd = [];
-const arraysDesc = [];
-const arraysNac = [];
-const arraysCant = [];
+
+// Definición de array con objetos
+const arrayDatosProd = [
+    {
+        arrcodigo : codigoProd,
+        arrDescripcion : descripcion,
+        arrNacionalidad : nacionalidad,
+        arrCantidad : cantidad
+    }
+];
+
+arrayDatosProd.pop();
 
 //Identificación
+function saludar() {
+    alert("Hola " + nombreIngresado + " puedes ingresar al menú.");
+    }    
 do {
-
-    nombre = prompt("Introduzca su nombre:");
-    if (nombre !== "") {
-        function saludar(nombre) {
-            alert("Hola " + nombre + " puedes ingresar al menú.");
-            saludar(nombre);
-        }    
+    nombreIngresado = prompt("Introduzca su nombre:").toUpperCase();
+    if (nombreIngresado !== "") {
+        saludar()
     }
-} while (nombre == "");
+} while (nombreIngresado == "");
 
 //Menú principal y ciclo de ingreso de datos
 
 opcionIngreso = parseInt(prompt("Ingreso de Prductos \n1.- Alta. \nOtro.- Salir"));
+
 if (opcionIngreso ==1) {
     do {
         codigoProd = prompt("Ingrese Código o escribí \"SALIR\" para termianr.");
         if (codigoProd.toUpperCase() !== "SALIR") {
             
-            descripcion = prompt("Ingrese descripción del Producto: ");
-            nacionalidad = prompt("Ingrese nacionalidad del Producto: ");
+            descripcion = prompt("Ingrese descripción del Producto: ").toUpperCase();
+            nacionalidad = prompt("Ingrese nacionalidad del Producto: ").toUpperCase();
             cantidad = parseInt(prompt("Ingrese cantidad del Producto: "));
     
             alert("Ud. Ingreso el siguiente producto:\n" + " Código: " + codigoProd + " Descripción: " + descripcion + " Nacionalidad: "+ nacionalidad + " Cantidad: " + cantidad);  
-                    
-            arraysCodProd.push(codigoProd); 
-            arraysDesc.push(descripcion.toUpperCase());
-            arraysNac.push(nacionalidad.toUpperCase());
-            arraysCant.push(cantidad);
+
+            arrayDatosProd.push(
+                {
+                    arrcodigo : codigoProd,
+                    arrDescripcion : descripcion,
+                    arrNacionalidad : nacionalidad,
+                    arrCantidad : cantidad  
+                }
+            );   
         } else {
             break;
         }
@@ -57,25 +69,28 @@ if (opcionIngreso ==1) {
 
 do {
 
-    let accion = prompt("Que información necesita: \n1.- Total de Codigos. \n2.- Cantidad de productos por Nacionalidad. \n3.- Total del Inventario. \n4.- SALIR.");
+    accion = prompt("Que información necesita: \n1.- Total de Codigos. \n2.- Cantidad de productos por Nacionalidad. \n3.- Total del Inventario. \n4.- SALIR.");
    
     if (accion == 1) {
-       alert("Cantidad de productos ingresada  " + arraysCodProd.length);
+       alert("Cantidad de productos ingresada  " + arrayDatosProd.length);
     } else {
        
         if (accion == 2) {
-           let ingreseNac = prompt("Ingrese Nacionalidad a buscar:");
-           const resultadoNac = arraysNac.filter((nacionalidad) => nacionalidad == ingreseNac.toUpperCase());
-           alert("Cantidad de pruductos con nacionalidad : " + ingreseNac + " " + resultadoNac.length);
-           console.log(resultadoNac);
-        } else {
+           ingreseNac = prompt("Ingrese Nacionalidad a buscar:").toUpperCase();
+         
+           let cantidadEncontrada = 0;
+
+            arrayDatosProd.forEach((nac) => {
+                if (nac.arrNacionalidad.includes(ingreseNac)) {
+                cantidadEncontrada++;
+                }
+            });
+             alert("Cantidad de pruductos con nacionalidad : " + ingreseNac + " " + cantidadEncontrada);           
+         } else {
    
            if (accion == 3) {
-               let totalCant = 0;
-               arraysCant.forEach(function(numero) {
-                   totalCant +=numero;
-               });
-               alert("Cantidad de pruductos en Inventario: " + totalCant);
+                const totalInventario = arrayDatosProd.reduce((acc,el) => acc + el.arrCantidad, 0);
+                alert("Cantidad de pruductos en Inventario: " + totalInventario);
             } else {
                  if (accion == 4) {
                      break;
@@ -83,11 +98,9 @@ do {
            }
        } 
    }
-   console.log(accion);
 } while (accion !== 4);
-
-
 alert("Fin de Operación");
+
 
 
 
